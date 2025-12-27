ed6ce977aeb1ecc455e88f8aaa823dc6445d7ada
HiFirsty999 <fc.firsty@gmail.com>
2025-12-27 13:12:04 +0700

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+var Sound = document.getElementById("Sound");
+var iShowSpeed = document.getElementById("iShowSpeed");
+
+let bool = true;
+
+iShowSpeed.style.visibility = 'hidden';
+
+function PlaySound() {
+    console.log("Played");
+    Sound.play();
+}
+
+function PauseSound() {
+    console.log("Paused");
+    Sound.pause();
+}
+
+function TryNotToLaugh() {
+    console.log("No, dont!");
+
+    if (bool == true) {
+       iShowSpeed.style.visibility = 'visible';
+    } else {
+        iShowSpeed.style.visibility = 'hidden';
+    }
+
+    bool = !bool;
+    console.log(bool);
+}
